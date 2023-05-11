4dc454f02e9e607ed97f90ee294c524f9ab548b6
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-05-11 18:32:51 +0200

    build color mode switch

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, Switch } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { useColorMode } from "@chakra-ui/color-mode";
 
 const NavBar = () => {
+  const { toggleColorMode } = useColorMode();
   return (
     <nav>
       <HStack justify="space-between" pr={3}>
         <Image src={logo} alt="logo" w="60px"></Image>
-        <Text>NavBar</Text>
+        <HStack>
+          <Switch colorScheme="green" onChange={toggleColorMode}></Switch>
+          <Text>Dark Mode</Text>
+        </HStack>
       </HStack>
     </nav>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ColorModeProvider } from "@chakra-ui/color-mode";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -9,7 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
