e7f4372ddcb97b11b65f99b22afa49200c62f006
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-05-11 16:43:49 +0200

    create responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"header"
+      "main"`,
+        lg: `"header header"
+      "aside main"`,
+      }}
+    >
+      <GridItem bg="orange.200" area="header">
+        Header
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="blue.200" area="aside">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="green.200" area="main">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
