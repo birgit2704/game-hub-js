86ac9ecf6cae74d8205617c3401cfdc1daafd38b
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-05-11 21:42:38 +0200

    implement error message

--- src/components/GameCardContainer.jsx
+++ src/components/GameCardContainer.jsx
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { SimpleGrid } from "@chakra-ui/react";
-import apiClient, { CanceledError } from "../services/api-client";
-import axios from "axios";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import axios, { CanceledError } from "axios";
 
 const GameCardContainer = () => {
   const [games, setGames] = useState([]);
+  const [error, setError] = useState("");
   useEffect(() => {
     axios
       .get("https://api.rawg.io/api/games?key=81b8071d70ca4486bd7392692f4b3e5b")
-      .then((res) => setGames(res.data.results));
+      .then((res) => {
+        setGames(res.data.results);
+        setError("");
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
   }, []);
 
   return (
-    <ul>
-      {games.map((g) => (
-        <li>{g.name}</li>
-      ))}
-    </ul>
+    <div>
+      {error && <Text color="red">{error}</Text>}
+      <ul>
+        {games.map((g) => (
+          <li>{g.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
